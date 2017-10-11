a8d526f4da143965aab7ac3cc32798506586e677
Michael.Staedtler <Michael.Staedtler@172.31.57.4>
2017-10-11 09:05:23 +0200

    Objekt erweitert

--- AbacusInterfaceExtender/src/ch/gruner/dbs/aie/businessobjects/InvoiceWV.java
+++ AbacusInterfaceExtender/src/ch/gruner/dbs/aie/businessobjects/InvoiceWV.java
@@ -3,23 +3,78 @@ package ch.gruner.dbs.aie.businessobjects;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.commons.collections4.ListUtils;
-
 public class InvoiceWV {
-	
+
 	public InvoiceWV(List<WVImportBooking> importBookings) {
 		this.importBookings = importBookings;
 	}
-	
+
 	private List<WVImportBooking> importBookings;
 	private Debitorenadresse adresse;
 	private String projektReferenz;
 	private Integer projektNummer;
 	private HashMap<Integer, HashMap<String, Double>> lineItems;
-	
-	private void test() {
-		
+
+	public List<WVImportBooking> getImportBookings() {
+		return importBookings;
 	}
-	 
-	
+
+	public void setImportBookings(List<WVImportBooking> importBookings) {
+		this.importBookings = importBookings;
+	}
+
+	public Debitorenadresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Debitorenadresse adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getProjektReferenz() {
+		return projektReferenz;
+	}
+
+	public void setProjektReferenz(String projektReferenz) {
+		this.projektReferenz = projektReferenz;
+	}
+
+	public Integer getProjektNummer() {
+		return projektNummer;
+	}
+
+	public void setProjektNummer(Integer projektNummer) {
+		this.projektNummer = projektNummer;
+	}
+
+	public HashMap<Integer, HashMap<String, Double>> getLineItems() {
+		if (importBookings != null) {
+			if (lineItems == null) {
+				lineItems = new HashMap<>();
+			}
+			HashMap<String, Double> innerMap;
+			for (WVImportBooking wvImportBooking : importBookings) {
+				Integer costCenter = wvImportBooking.getCostCenter();
+				String profile = wvImportBooking.getProfile();
+				innerMap = lineItems.get(costCenter);
+				if (innerMap == null) {
+					innerMap = new HashMap<>();
+				}
+				Double oldAmount = innerMap.get(profile);
+				if (oldAmount == null) {
+					oldAmount = 0.0d;
+				}
+				Double newAmount = oldAmount + wvImportBooking.getCost();
+				innerMap.put(profile, newAmount);
+				lineItems.put(costCenter, innerMap);
+			}
+
+		}
+		return lineItems;
+	}
+
+	public void setLineItems(HashMap<Integer, HashMap<String, Double>> lineItems) {
+		this.lineItems = lineItems;
+	}
+
 }
